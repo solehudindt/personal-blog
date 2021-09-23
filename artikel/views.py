b99b346adae6ed00843a74a65b9079d2b9588e55
solehudindt <solehudindt@gmail.com>
2021-09-23 12:40:03 +0700

    category fixed and related artikl

--- artikel/views.py
+++ artikel/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Artikel
+import random
 
 class ArtikelListView(ListView):
     model = Artikel
@@ -22,10 +23,29 @@ class ArtikelKategoriView(ListView):
     ordering = ['-published']
 
     def get_queryset(self):
-        artikel_kategori = self.model.objects.filter(kategori=self.kwargs['kategori'])
-        print(artikel_kategori)
+        self.queryset = self.model.objects.filter(kategori=self.kwargs['kategori'])
+        return super().get_queryset()
+
+    def get_context_data(self,*args,**kwargs):
+        kategori_list = self.model.objects.values_list('kategori', flat=True).distinct().exclude(
+            kategori=self.kwargs['kategori'])
+        self.kwargs.update({'kategori_list':kategori_list})
+        kwargs = self.kwargs
+        return super().get_context_data(*args,**kwargs)
 
 class ArtikelDetailView(DetailView):
     model = Artikel
     template_name = "artikel/artikel_detail.html"
     context_object_name = 'artikel'
+
+    def get_context_data(self,*args,**kwargs):
+        related_artk = list(self.model.objects.all().exclude(id=self.object.id))
+        if len(related_artk) < 3:
+            item_count = len(related_artk)
+        else:
+            item_count = 3
+        random_items = random.sample(related_artk, item_count)
+        self.kwargs.update({'related_artikel':random_items})
+
+        kwargs = self.kwargs
+        return super().get_context_data(*args,**kwargs)
